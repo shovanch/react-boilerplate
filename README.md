b85c884b06e4e49000d27b71f12eac8c22dd4851
Shovan Chatterjee <hello@shovanch.com>
2019-08-06 01:40:12 +0530

    Add absolute imports

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+
+  ## Absolute imports available
+  the base is **'src'** <br>
+  That means all the imports can start from **'src/....' now**
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import './App.scss';
+import Img from 'assets/images/Inn.svg';
 
 class App extends Component {
   render() {
@@ -8,7 +9,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>Hi, I am Shovan - The developer</h1>
-          <h2>Let's Code</h2>
+          <h2>Let Code</h2>
+          <img src={Img} alt="background" />
         </header>
       </div>
     );
