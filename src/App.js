8b910d2e4bf77d792a47f1c9c9834beaa7196032
Shovan Chatterjee <hello@shovanch.com>
2020-01-14 12:41:19 +0530

    Update package versions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
-import './App.scss';
-import Img from 'assets/images/Inn.svg';
+import React, { Component } from "react";
+import Img from "assets/images/Inn.svg";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Hi, I am Shovan - The developer</h1>
+          <h1>Hi, I am Shovan - developer</h1>
           <h2>Let Code</h2>
           <img src={Img} alt="background" />
         </header>
